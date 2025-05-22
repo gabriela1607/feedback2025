540bfe90000508decc8e2aa8d335b105d5e5b074
gabrielacristinazilioherculano <gabrielacristinazilioherculano@gmail.com>
2025-05-22 02:38:00 +0000

    ajustes de responsividade dos botões de avaliação no primeiro card

--- src/App.jsx
+++ src/App.jsx
@@ -32,11 +32,11 @@ export function App () {
       <p className="text-sm text-light-grey mb-6 leading-1 ">Please let us know how we did with your support request. All feedback is appreciated to help us improve our offering!</p>
 
       <div className="flex justify-between mb-6 ">
-        <input type="button" value={1} className="bg-dark-blue w-10.5 h-10.5 rounded-full text-medium-grey text-sm font-bold focus:bg-medium-grey focus:text-white sm:w-13 sm:h-13 hover:bg-orange hover:text-white hover: cursor-pointer"  onClick={() => handleMudarNotaAvaliação(1)}/>
-        <input type="button" value={2} className="bg-dark-blue w-10.5 h-10.5 rounded-full text-medium-grey text-sm font-bold focus:bg-medium-grey focus:text-white sm:w-13 sm:h-13 hover:bg-orange hover:text-white hover: cursor-pointer" onClick={() => handleMudarNotaAvaliação(2)}/>
-        <input type="button" value={3} className="bg-dark-blue w-10.5 h-10.5 rounded-full text-medium-grey text-sm font-bold focus:bg-medium-grey focus:text-white sm:w-13 sm:h-13 hover:bg-orange hover:text-white hover: cursor-pointer" onClick={() => handleMudarNotaAvaliação(3)}/>
-        <input type="button" value={4} className="bg-dark-blue w-10.5 h-10.5 rounded-full text-medium-grey text-sm font-bold focus:bg-medium-grey focus:text-white sm:w-13 sm:h-13 hover:bg-orange hover:text-white hover: cursor-pointer" onClick={() => handleMudarNotaAvaliação(4)}/>
-        <input type="button" value={5} className="bg-dark-blue w-10.5 h-10.5 rounded-full text-medium-grey text-sm font-bold focus:bg-medium-grey focus:text-white sm:w-13 sm:h-13 hover:bg-orange hover:text-white hover: cursor-pointer" onClick={() => handleMudarNotaAvaliação(5)}/>
+        <input type="button" value={1} className="bg-dark-blue w-10.5 h-10.5 rounded-full text-medium-grey text-sm font-bold focus:bg-medium-grey focus:text-white  hover:bg-orange hover:text-white sm:w-13 sm:h-13 hover: cursor-pointer"  onClick={() => handleMudarNotaAvaliação(1)}/>
+        <input type="button" value={2} className="bg-dark-blue w-10.5 h-10.5 rounded-full text-medium-grey text-sm font-bold focus:bg-medium-grey focus:text-white  hover:bg-orange hover:text-white sm:w-13 sm:h-13 hover: cursor-pointer" onClick={() => handleMudarNotaAvaliação(2)}/>
+        <input type="button" value={3} className="bg-dark-blue w-10.5 h-10.5 rounded-full text-medium-grey text-sm font-bold focus:bg-medium-grey focus:text-white  hover:bg-orange hover:text-white sm:w-13 sm:h-13 hover: cursor-pointer" onClick={() => handleMudarNotaAvaliação(3)}/>
+        <input type="button" value={4} className="bg-dark-blue w-10.5 h-10.5 rounded-full text-medium-grey text-sm font-bold focus:bg-medium-grey focus:text-white  hover:bg-orange hover:text-white sm:w-13 sm:h-13  hover: cursor-pointer" onClick={() => handleMudarNotaAvaliação(4)}/>
+        <input type="button" value={5} className="bg-dark-blue w-10.5 h-10.5 rounded-full text-medium-grey text-sm font-bold focus:bg-medium-grey focus:text-white  hover:bg-orange hover:text-white sm:w-13 sm:h-13 not-target:hover: cursor-pointer" onClick={() => handleMudarNotaAvaliação(5)}/>
       </div>
 
       <button onClick={handleSubmit} className="bg-orange w-full uppercase tracking-1 font-bold rounded-3xl text-sm py-3 hover:bg-white hover:text-orange hover:cursor-pointer">Submit</button>
